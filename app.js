1021a9e3e7e685c573744534b435d374527925b0
Lucia Belmonte <Lucia.belmonte@hotmail.com.ar>
2023-09-29 17:20:40 -0300

    Tarea peticiones, respuestas y templates

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// `var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// `
 var app = express();
 
 // view engine setup
